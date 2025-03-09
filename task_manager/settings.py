d6ddacc03c4e700a8a278b51d5b31d4952c102c0
sajjadzolghadr77 <sajjadzolghadr777@gmail.com>
2025-03-09 23:23:24 +0330

    add user and test register and login and refresh

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,22 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+
+#add settings for JSON Web Token (JWT)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICAION_CLASSES':(
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ),
+}
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':
+    timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME':
+    timedelta(days=7),
+}
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,27 @@
+# add view for task model 
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+# add view for user
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from rest_framework import status
 
+# add view for task model 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+# add view for user
+@api_view(['POST'])
+def register_user(request):
+    serializer = UserSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
 
 
 
